dfd500f628f464dbc11641635314609332ddc605
AlexKherson <q0992358472@gmail.com>
2022-06-01 11:56:11 +0300

    initial-create

---
+++ BasicForum/BasicForum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BasicForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
