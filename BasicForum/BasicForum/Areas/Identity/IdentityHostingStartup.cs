fb6916ddcd57bb47223b7cccd59677d109759ead
AlexKherson <q0992358472@gmail.com>
2022-06-01 12:43:26 +0300

    added auth

---
+++ BasicForum/BasicForum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BasicForum.Areas.Identity.Data;
+using BasicForum.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BasicForum.Areas.Identity.IdentityHostingStartup))]
+namespace BasicForum.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BasicForumDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BasicForumDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BasicForumDBContext>();
+            });
+        }
+    }
+}
